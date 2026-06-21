7472e293f65d5bf73e08e22ede82d7862d3e60cd
SeanPala <73197130+SeanPala@users.noreply.github.com>
2026-06-21 16:07:38 -0700

    deletes

--- server/prisma/migrations/20260220234633_add_social_profiles/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "instagramUrl" TEXT,
-ADD COLUMN     "linkedinUrl" TEXT;
--- server/prisma/migrations/20260325000000_add_verification_data/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable: add verificationData JSON column to Application
-ALTER TABLE "Application" ADD COLUMN "verificationData" JSONB;
